dfe07c8ea04e65becf4d0badb937d4877e582f6b
Jase Bell <jasebell@gmail.com>
2015-07-22 23:06:31 +0100

    Polish up the load-questions function, create the spark/join and extract the department name frequencies per member.

--- src/mlas/core.clj
+++ src/mlas/core.clj
@@ -9,16 +9,18 @@
             )
   (:gen-class))
 
-(def members-path (io/resource "members.json"))
-(def questions-path (io/resource "/questions/"))
+(def members-path "/Users/Jason/work/data/niassembly/members.json")
+(def questions-path "/Users/Jason/work/data/niassembly/questions/")
 (def api-questions-by-member "http://data.niassembly.gov.uk/questions.asmx/GetQuestionsByMember_JSON?personId=")
 
 (defn load-members [sc filepath] 
   (->> (spark/whole-text-files sc filepath)
        (spark/flat-map (s-de/key-value-fn (fn [key value] 
-                                            (-> (json/read-str value :key-fn (fn [key] (-> key
-                                                                                           str/lower-case
-                                                                                           keyword)))
+                                            (-> (json/read-str value 
+                                                               :key-fn (fn [key] 
+                                                                         (-> key
+                                                                             str/lower-case
+                                                                             keyword)))
                                                 (get-in [:allmemberslist :member])))))
        (spark/map-to-pair (fn [rec] (spark/tuple (:personid rec) rec)))))
 
@@ -28,15 +30,27 @@
                                        (spit (str questions-path key ".json") 
                                              (slurp (str api-questions-by-member key))))))))
 
-(defn load-questions [members] 
-  (->> members
-       (spark/map-to-pair (s-de/key-value-fn (fn [k v] 
-                                       (spark/tuple k 
-                                                    (-> (json/read-str 
-                                                         (slurp (str questions-path k ".json")) 
-                                                         :key-fn (fn [key] 
-                                                                   (-> key str/lower-case keyword))) 
-                                                        (get-in [:questionslist :question]))))))))
+(defn load-questions [sc questions-path] 
+  (->> (spark/whole-text-files sc questions-path)
+       (spark/flat-map (s-de/key-value-fn (fn [key value] 
+                                            (-> (json/read-str value
+                                                 :key-fn (fn [key] 
+                                                           (-> key 
+                                                               str/lower-case 
+                                                               keyword)))
+                                                (get-in [:questionslist :question])))))
+       (spark/map-to-pair (fn [rec] (spark/tuple (:tablerpersonid rec) rec)))
+       (spark/group-by-key)))
+
+(defn join-members-questions [members-rdd questions-rdd]
+  (spark/join members-rdd questions-rdd))
+
+(defn department-frequencies-rdd [members-questions-rdd] 
+  (->> members-questions-rdd
+       (spark/map-to-pair 
+        (s-de/key-val-val-fn (fn [key member questions]
+                               (let [freqmap (map (fn [question] (:departmentname question)) questions)]
+                                 (spark/tuple key (frequencies freqmap))))))))
 
 (comment
   (def c (-> (conf/spark-conf)
@@ -44,4 +58,5 @@
              (conf/app-name "niassemblydata-sparkjob")))
   (def sc (spark/spark-context c))
 
-  )
+)
+ 
